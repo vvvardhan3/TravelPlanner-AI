2fe90374f79b11796d1c36df66f124c775206032
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-01 11:06:42 +0530

    introduced react rouder dom and create a new create-trip page

---
+++ src/create-trip/index.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+function CreateTrip() {
+  return (
+    <>
+    <h2>Create-Trip</h2>
+    </>
+    
+  )
+}
+
+export default CreateTrip
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import CreateTrip from './create-trip'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
