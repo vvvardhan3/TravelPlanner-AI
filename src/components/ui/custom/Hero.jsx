1ef2e9b0fe2039fc1e5a636f08906044434ede88
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-09 22:07:54 +0530

    Update Hero Section

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+import { Button } from "../button"
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[60px] text-center mt-16">
+        <span className="text-blue-500">Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your FingerTips
+      </h1>
+      <p className="text-xl text-gray-500 text-center">Your personal trip planner and travel curator, creating custom Itineraries tailored to your interests and budget</p>
+
+      <Link to={'/create-trip'}> 
+      <Button>Get Started, It Is Free..</Button>
+      </Link>
+     
+
+    </div>
   )
 }
 
